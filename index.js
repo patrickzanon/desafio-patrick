c6fce568fcb5b8853b5c40666c5a6873bb1d5373
patrickzanon <patrickeduardozanonmaciel@gmail.com>
2024-12-03 19:31:58 -0400

    quarto commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("sddsdds")
+
+let HeroiXp =["Hulk", 11000 ]
+
+if(HeroiXp[1] <= 1000 ){
+    console.log("O Heroi de nome: " + HeroiXp[0] + " está no nível de Ferro" )
+}else if(HeroiXp[1] <= 2000){
+    console.log("O Heroi de nome: " + HeroiXp[0] + " está no nível de Bronze")
+}else if(HeroiXp[1] <= 5000){
+    console.log("O Heroi de nome: " + HeroiXp[0] + " está no nível de Prata")
+}else if(HeroiXp[1] <= 7000){
+    console.log("O Heroi de nome: " + HeroiXp[0] + " está no nível de Ouro")
+}else if(HeroiXp[1] <= 8000){
+    console.log("O Heroi de nome: " + HeroiXp[0] + " está no nível de Platina")
+}else if(HeroiXp[1] <= 9000){
+    console.log("O Heroi de nome: " + HeroiXp[0] + " está no nível de Ascendente")
+}else if(HeroiXp[1] <= 10000){
+    console.log("O Heroi de nome: " + HeroiXp[0] + " está no nível de Imortal")
+}else{
+    console.log("O Heroi de nome: " + HeroiXp[0] + " está no nível de Radiante")
+}
+
